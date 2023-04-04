782e46a08c162d46c82e436bdcba424c422e0557
TwentyFiend3646 <alejandro.sedeno.dw@immune.institute>
2023-04-04 18:53:37 +0200

    added

--- js/pisos.js
+++ js/pisos.js
@@ -53,7 +53,7 @@ function mostrarCiudades(data){
     for(let datos of data){
             let nuevoContenido=`
                 <div class="ciudad">
-                    <a src="#upper"><img class="img_seleccion" src="${datos.imagen}" alt="${datos.name}.jpg" width="400px" onclick="seleccion('${datos.id}'),pintarPisos('${datos.id}')"><a>
+                    <img class="img_seleccion" src="${datos.imagen}" alt="${datos.name}.jpg" width="400px" onclick="seleccion('${datos.id}'),pintarPisos('${datos.id}')"><a src="#upper"></a>
                     <h3 class="name">${datos.name}</h3>
                 </div>
             `
